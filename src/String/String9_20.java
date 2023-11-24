f8edbc724cb30b30d36b123bcfc4e96f5cc82f8c
AshkanTheBoy <bedrok@bk.ru>
2023-11-25 00:13:54 +0300

    Homework

--- src/String/String9_20.java
+++ src/String/String9_20.java
@@ -6,9 +6,11 @@ public class String9_20 {
     третьего и последнего символа.
     */
     public static void main(String[] args) {
+        //Через индексы строки
         String a = "12345";
-        String c = a.substring(3-1,4-1)+""+a.substring(a.length()-1);
+        String c = a.charAt(3-1)+""+a.charAt(a.length()-1);
         System.out.println(c);
+        //Через индексы массива символов
         char[] b = a.toCharArray();
         c = b[3-1]+""+b[b.length-1];
         System.out.println(c);
