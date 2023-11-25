95a4d3e36a6c9c2280a7b3546f4be6fb0102e785
AshkanTheBoy <bedrok@bk.ru>
2023-11-25 03:42:01 +0300

    Homework

--- src/String/String9_70.java
+++ src/String/String9_70.java
@@ -4,14 +4,17 @@ public class String9_70 {
     public static void main(String[] args) {
         String word = "qwertyuioEEEEE";
         System.out.println(word);
+        //слово в ловеркейс
         word = word.toLowerCase();
         System.out.println(word);
+        //слово в буквенный массив
         char[] wordInChar = word.toCharArray();
         int count = 0;
         char[] vowels = {'a','e','i','o','u','y',};
-        for (int i = 0; i<word.length(); i++) {
-            for (int j = 0; j<vowels.length; j++){
-                if (wordInChar[i]==vowels[j]) {
+        //в цикле сравниваем букву и "гласную", если совпадают - +1 гласная
+        for (char wordChar : wordInChar) {
+            for (char vowel : vowels) {
+                if (wordChar==vowel) {
                     count++;
                 }
             }
